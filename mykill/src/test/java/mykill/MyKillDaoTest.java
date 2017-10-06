4d9da160841626d267084d882ee5549d82bd0571
homehowe <17729386712@163.com>
2017-10-06 09:50:44 +0800

    junit test
    test

--- mykill/src/test/java/mykill/MyKillDaoTest.java
+++ mykill/src/test/java/mykill/MyKillDaoTest.java
@@ -1,7 +1,5 @@
 package mykill;
 
-import static org.junit.Assert.fail;
-
 import java.util.Date;
 import java.util.List;
 
---
+++ mykill/src/test/java/mykill/SuccessKilledDaoTest.java
@@ -0,0 +1,37 @@
+package mykill;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import howe.mykill.dao.MyKillDao;
+import howe.mykill.dao.SuccessKilledDao;
+import howe.mykill.entity.Mykill;
+import howe.mykill.entity.SuccessKill;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+	@Autowired
+	private SuccessKilledDao successKilledDao;
+	
+	@Test
+	public void insertSuccessKilled() throws Exception {
+		int insertCount = successKilledDao.insertSuccessKilled(1000, 18888888888L);
+		
+		System.out.println("success:" + insertCount);
+	}
+
+	@Test
+	public void queryByIdWithmykill() throws Exception {
+		SuccessKill successKill = successKilledDao.queryByIdWithmykill(1000, 18888888888L);
+		
+		System.out.println(successKill.getSeckill());
+	}
+
+}
